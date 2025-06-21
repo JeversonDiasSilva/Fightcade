a4d74f8deeac10d4b68dd909cfdd4651ad805787
Jeverson Dias da Silva <jeversondiasdasilva@gmail.com>
2025-06-21 11:01:20 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,16 +3,16 @@
 # Editor Jeverson Dias da Silva
 # Youtube/@JCGAMESCLASSICOS
 # Script de instalação"ONLINE" do sistema "FIGHTCADE 2"
-#wget https://raw.githubusercontent.com/JeversonDiasSilva/Fightcade/main/img/fightcade.png -O /usr/share/emulationstation/themes/es-theme-carbon/art/logos/fightcade.png
 url="https://github.com/JeversonDiasSilva/Fightcade/releases/download/v1.0/run.jc" > /dev/null 2>&1
 dep="https://github.com/JeversonDiasSilva/streetfighterv/releases/download/v1.0/xdotool" > /dev/null 2>&1
-
-# Nome do script que será baixado
+url_logo="https://raw.githubusercontent.com/JeversonDiasSilva/Fightcade/main/img/fightcade.png"
 squash=$(basename "$url")
+
 
 # Baixando o script e a dependência xdotool
 wget "$url" -O "$squash" > /dev/null 2>&1
 wget "$dep" -O xdotool > /dev/null 2>&1
+wget "$url_logo" -O /usr/share/emulationstation/themes/es-theme-carbon/art/logos/fightcade.png
 
 # Tornando ambos executáveis
 chmod +x "$squash"
